177d2869a83a33fbf426aaa0e0f28048de24654b
Alex <al_dyutkin@mail.ru>
2022-12-16 19:47:44 +0300

    Загрузка ДЗ

---
+++ tests/google_test.py
@@ -0,0 +1,17 @@
+from selene.support.shared import browser
+from selene import command, have
+import os
+
+
+def test_google_pic():
+    # browser.open('https://www.cian.ru/')
+    # browser.element('._25d45facb5--link--rqF9a').click()
+
+    browser.open('https://feature-electronic-queues.iseeds-frontend.dev.smartseeds.ru/')
+    # browser.element("//span[contains(text(),'Вход')]").double_click()
+    # browser.element('[class*=landing-header__button-value]:nth-child(2)').double_click()
+
+
+
+
+
